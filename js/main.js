79ac56321497fc60e83c325c42b0fcef2b8d1855
Anirban2001 <deyanirban988@gmail.com>
2022-07-30 04:20:59 +0530

    left and right scrolling added

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
         $iconbar.toggleClass('toggle-click');
     })
+
+    //owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next'),]
+    });
 })
