6157eb9f01e90c0e76d1c02c8ada68f85cbb0737
Anirban2001 <deyanirban988@gmail.com>
2022-07-30 15:21:17 +0530

    front page made

--- js/main.js
+++ js/main.js
@@ -1,22 +1,50 @@
-$(document).ready(function(){
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
 
     $nav = $('.nav');
-    $togglecollapse = $('.toggle-collapse');
+    $toggleCollapse = $('.toggle-collapse');
     $iconbar = $('.iconbar');
 
-    // click event on toggle menu
-    $togglecollapse.click(function(){
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
         $iconbar.toggleClass('toggle-click');
     })
 
-    //owl-crousel for blog
+    // owl-crousel for blog
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        dots:false,
-        nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next'),]
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
-})
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
